e15ae949434d36b192daae0b91fdc41f84541f94
Cesar Chavez <91764968+cesarchavez15@users.noreply.github.com>
2023-12-03 22:15:39 -0800

    Attempt 1
    minor success

--- Main.java
+++ Main.java
@@ -7,8 +7,8 @@ class Product{
     Double price;
 
     Product(){
-        productName = "Lays";
-        price = 3.50;
+        productName = null;
+        price = null;
     }
 
     Product(String productName, Double price){
@@ -24,13 +24,96 @@ class Main{
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
+        Boolean x = true;
 
         ArrayList<Product> menu = new ArrayList<Product>();
-        while(true){
-            System.out.println("Choose one:\n1.Add to Grocery list at" + 
+        while(x == true){
+            System.out.println("Choose one:\n1.Add to Grocery list at " + 
             "any given index\n2.Remove from Grocery at any given index\n" +
             "3.Display the grocery List\n4.Show the current total of all the" +
-            "in the Grocery List\n5.quit the program");
+            " Products in the Grocery List\n5.quit the program");
+            Integer option = in.nextInt();
+
+            if (option == 1){   
+                Add(menu);
+            }
+            else if(option == 2){
+                Remove(menu);
+            }
+            else if(option == 3){
+                Display(menu);
+            }
+            else if(option== 4){
+                Total(menu);
+            }
+            else if(option == 5){
+                x = false;
+            }
+            else{
+                System.out.println("Give correct number");
+            }
         }
+    }
+
+    public static void Add(ArrayList<Product> menu){
+        Scanner in = new Scanner(System.in);
+        Product newProduct = new Product();
+        Boolean compare = false;
+
+        System.out.println("What product you like to add?");
+        String choice1 = in.nextLine();
+        System.out.println("How much is it?");
+        Double choice2 = in.nextDouble();
+        int index = 0;
+        for(int i = 0; i < menu.size(); i++){
+            if(choice1.equals(menu.get(i).productName)){
+                compare = true;
+            }
+            else{
+                compare = false;
+            }
+        }
+        if(choice1.length() > 40){
+            System.out.println("Inputs cannot be greater tha 40 characters in" +
+                                 " length");
+        }
+        else if(compare == true){
+            System.out.println("Inputs cannot be duplicate items");
+        }
+        else{
+            newProduct.productName = choice1;
+            newProduct.price = choice2;
+            try{
+                System.out.println("What index would you like to add to");
+                index = in.nextInt();
+            }catch(Exception e){
+                System.out.println("Please put in a valid index");
+            }
+                menu.add(index, newProduct);
+        }
+    }
+    public static void Remove(ArrayList<Product> menu){
+        Scanner in = new Scanner(System.in);
+        Product newProduct = new Product();
+
+        System.out.println("What index would you like to remove?");
+        int delete = in.nextInt();
+        menu.remove(delete);
+    }
+    public static void Display(ArrayList<Product> menu){
+        for(int i = 0; i < menu.size(); i++){
+                String out = String.format("\nProduct Name: %s\nPrice: %.2f\n" +
+                                            "Index: %d",
+                                menu.get(i).productName, menu.get(i).price, i);
+                System.out.println(out);
+            
+        }
+    }
+    public static void Total(ArrayList<Product> menu){
+        double total = 0;
+        for(int i = 0; i < menu.size(); i++){
+            total += menu.get(i).price;
+        }
+        System.out.println("%" + total);
     }
 }
